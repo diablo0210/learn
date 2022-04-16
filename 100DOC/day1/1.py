79d5cee9f8523c51b4b5a48655277f14253adb7f
Vivek Sharma <sharmavivek.iit@gmail.com>
2022-04-16 15:20:43 +0530

    day1

---
+++ 100DOC/day1/1.py
@@ -0,0 +1,16 @@
+print("Hello " + input("What is your name?\n"))
+# functions within functions
+
+print(ord('S'))
+print(ord('n'))
+print(ord('e'))
+print(ord('h'))
+print(ord('a'))
+
+
+print(len(input("Enter your name? ")))
+# to print the number of letters in a name
+
+name = input("What is your name?\n")
+print(len(name))
+# using variables
---
+++ 100DOC/day2/1.py
@@ -0,0 +1,9 @@
+# tip calculator
+
+x = input("What is the total bill amount?\n")
+y = input("How many people to split the bill?\n")
+z = input("What percentage tip would you like to give?\n")
+a = float(x) * float(z) / 100 #total tip
+b = float(x) + float(a) #total amount after tip
+c = float(b)/float(y) #final amount per person
+print("Each person should pay:\n", c)
