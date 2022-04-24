11e7ebc06de09f8a3bbada9c98548ecccebdb326
Vivek Sharma <sharmavivek.iit@gmail.com>
2022-04-24 18:04:43 +0530

    100DOC

---
+++ 100DOC/day6/6_1.py
@@ -0,0 +1,14 @@
+# Defining and Calling Python Functions
+
+print("Hello")
+print(len("Hello"))
+
+# Creating/defining our own functions using 'def'
+def my_function():
+    print("Hello")
+    print("bye")
+# we have defined a new function, but it won't do anything until we call it
+
+# Calling the function
+my_function()
+
